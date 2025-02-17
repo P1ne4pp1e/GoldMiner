3eef3a21711c125fd9e41efb556827b6b44bf850
P1ne4pp1e <mabolong2006@outlook.com>
2025-02-17 22:26:39 +0800

    202502172226

--- main.cpp
+++ main.cpp
@@ -9,14 +9,21 @@
 #define HEIGHT 240
 #define WIDTH 320
 
-const double scaleFactor = 3.5;
+#define SET_SINGLE_FONT(height, width, fontName, fontPath) \
+do { \
+AddFontResourceEx(fontPath, FR_PRIVATE, 0); \
+settextstyle(height, width, _T(fontName)); \
+} while(0)
+
+
+const double scaleFactor = 2.0;
 
 using namespace std;
 namespace fs = std::filesystem;
 
 void loadingImage(const vector<Images>& images) {
     for (int i = 0; i < images.size(); i++) {
-        loadimage(images[i].name, IMG_BG_START_MENU, images[i].width * scaleFactor, images[i].height * scaleFactor);
+        loadimage(images[i].name, _T(IMG_BG_START_MENU), images[i].width * scaleFactor, images[i].height * scaleFactor);
     }
 }
 
@@ -24,8 +31,14 @@ int main() {
     loadingImage(images);
 
     initgraph(WIDTH * scaleFactor, HEIGHT * scaleFactor);
-    outtext("Hello World!");
+    outtext(_T("Hello World!"));
     putimage(0, 0, &bg_start_menu);
+
+    SET_SINGLE_FONT(20 * scaleFactor, 0, "Kurland", "fonts/Kurland.ttf");
+    setbkmode(TRANSPARENT);
+    settextcolor(RGB(255, 255, 0));
+    outtextxy(35 * scaleFactor, 155 * scaleFactor, _T("Start Game"));
+    outtextxy(35 * scaleFactor, 175 * scaleFactor, _T("High Score"));
 
     getch();
     closegraph();
