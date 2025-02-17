cf8f1f1ba909ac797ae04921609750e44cb94a88
P1ne4pp1e <mabolong2006@outlook.com>
2025-02-17 21:05:57 +0800

    202502172106

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <graphics.h>
+#include <conio.h>
+
+#define HEIGHT 240
+#define WIDTH 320
+
+int main() {
+    initgraph(WIDTH, HEIGHT);
+    outtext("Hello World!");
+
+    getch();
+    closegraph();
+
+    return 0;
+}
